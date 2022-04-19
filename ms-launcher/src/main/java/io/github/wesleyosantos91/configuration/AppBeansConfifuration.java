9806b33622f71706450554c1859af3c294196649
Wesley Oliveira Santos <wesleyosantos91@gmail.com>
2022-04-18 23:56:29 -0300

    feat: configurando projeto

---
+++ ms-launcher/src/main/java/io/github/wesleyosantos91/configuration/AppBeansConfifuration.java
@@ -0,0 +1,7 @@
+package io.github.wesleyosantos91.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppBeansConfifuration {
+}
---
+++ ms-launcher/src/test/java/io/github/wesleyosantos91/ApplicationTests.java
@@ -0,0 +1,14 @@
+package io.github.wesleyosantos91;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Application.main(new String[] {});
+    }
+
+}
