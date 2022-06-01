8e1ed00c769ec5d16aaa60bb5f467374a0d17339
Wesley Oliveira <wesleyosantos91@gmail.com>
2022-05-31 22:36:07 -0300

    feat: atualizado as dependencia

--- infrastructure/datastore-mysql/src/test/resources/load-database.sql
+++ infrastructure/datastore-mysql/src/test/resources/load-database.sql
@@ -1,2 +1,2 @@
-INSERT INTO tb_person
-VALUES (null, '56844030932', '1972-09-12', 'guilherme.gomes@hotmail.com', 'Guilherme Machado Gomes' );
+INSERT INTO tb_person(cpf, date_of_birth, email, name)
+VALUES ('56844030932', '1972-09-12', 'guilherme.gomes@hotmail.com', 'Guilherme Machado Gomes' );
