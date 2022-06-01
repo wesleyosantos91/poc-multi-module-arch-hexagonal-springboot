8b2d607cd59bf0f1c90389ae6f3c3cf67287a2ac
Wesley Oliveira Santos <wesleyosantos91@gmail.com>
2022-06-01 00:42:30 -0300

    feat: implementação de teste de integração api

--- ms-launcher/src/test/java/io/github/wesleyosantos91/api/v1/controller/PersonControllerIntegrationTest.java
+++ ms-launcher/src/test/java/io/github/wesleyosantos91/api/v1/controller/PersonControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package io.github.wesleyosantos91.api.v1.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import br.com.six2six.fixturefactory.Fixture;
 import br.com.six2six.fixturefactory.loader.FixtureFactoryLoader;
@@ -8,20 +9,18 @@ import io.github.wesleyosantos91.Application;
 import io.github.wesleyosantos91.api.v1.request.PersonRequest;
 import io.github.wesleyosantos91.api.v1.response.PersonResponse;
 import io.github.wesleyosantos91.configuration.AppBeansConfig;
-import io.github.wesleyosantos91.ports.api.PersonServicePort;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
@@ -47,7 +46,7 @@ class PersonControllerIntegrationTest {
     }
 
     @Test
-    @DisplayName("[integration] - should created one personDomain and return id 2")
+    @DisplayName("[integration] - should created one person and return id 2")
     void create() {
         PersonRequest personRequest = Fixture.from(PersonRequest.class).gimme("create");
         ResponseEntity<PersonResponse> response =
@@ -59,7 +58,7 @@ class PersonControllerIntegrationTest {
     }
 
     @Test
-    @DisplayName("[integration] - should return a personDomain with id equals 1")
+    @DisplayName("[integration] - should return a person with id equals 1")
     void getById() {
         ResponseEntity<PersonResponse> response = restTemplate.getForEntity("http://localhost:" + port + "/v1/persons/{id}", PersonResponse.class, 1);
         PersonResponse personResponse = response.getBody();
@@ -74,5 +73,23 @@ class PersonControllerIntegrationTest {
         List<PersonResponse> personResponses = List.of(Objects.requireNonNull(response.getBody()));
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertFalse(personResponses.isEmpty());
+    }
+
+    @Test
+    @DisplayName("[integration] - should update one person and return email change 'wesleyosantos91@gmail.com' ")
+    void update() {
+        PersonRequest personRequest = Fixture.from(PersonRequest.class).gimme("update");
+        ResponseEntity<PersonResponse> response = restTemplate.exchange("http://localhost:" + port + "/v1/persons/1", HttpMethod.PUT, new HttpEntity<>(personRequest), PersonResponse.class);
+        PersonResponse personResponse = response.getBody();
+        assertEquals("wesleyosantos91@gmail.com", personResponse.getEmail());
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("[integration] - should delete one person with id 1")
+    void delete() {
+        ResponseEntity<Void> response = restTemplate.exchange("http://localhost:" + port + "/v1/persons/1", HttpMethod.DELETE, HttpEntity.EMPTY, Void.class);
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+
     }
 }
